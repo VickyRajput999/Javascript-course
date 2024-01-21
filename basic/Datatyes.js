1990ae151cf8d2bd5d113b0c2715ded924f231e6
VickyRajput999 <154819926+VickyRajput999@users.noreply.github.com>
2024-01-21 11:56:00 +0000

    dataype and variables

---
+++ basic/Datatyes.js
@@ -0,0 +1,14 @@
+// Number => 2 to power 53
+// Bigint => large no
+// String => "vicky"
+// Boolean => true/false
+// null => standolne value // jb value khali hai //  null ek object
+// undefined => jb aapne value decide nhi ki hai // ye ek type hai
+// symbol => uniqueness 
+
+
+//---typeof => kisi value ka datatype check krne k liye use krte hai 
+
+
+
+//---Objects---//
--- basic/variables.js
+++ basic/variables.js
@@ -1,1 +1,7 @@
-console.log("Shivani")
+const name ="Vicky" //const variable hume allow nhi krta uski value change krne ko
+let password =1234 //let variable new version hai var ka
+var email = "vckthakur277@gmail.com" //is use nhi krna chahiye kyunki isme block scope or functio scope ka issue aata hai
+
+//console.table() sbhi values ko ek bar me print krvane ka kam krta hai 
+
+console.table([name,password,email])
